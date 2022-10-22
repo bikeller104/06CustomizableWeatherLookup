1a504cf39a0f4978ca9b0d8d7d546ad31d124135
Brent Keller <bikeller104@gmail.com>
2022-10-22 12:25:08 -0600

    found the entries for noon each day

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -55,13 +55,20 @@ function fetchWeather(lat,lon)
         // });
         console.log(JSON.stringify(data.list[0],null,4));
         console.log(data.list[0].dt_txt);
-        console.log(data.list[1].dt_txt);
-        console.log(data.list[2].dt_txt);
-        console.log(data.list[3].dt_txt);
-        console.log(data.list[4].dt_txt);
-        console.log(data.list[5].dt_txt);
+        console.log(data.list[1].dt_txt); //tomorrow
+        console.log(data.list[9].dt_txt); //the day after tomorrow
+        console.log(data.list[17].dt_txt); //three days from now
+        console.log(data.list[25].dt_txt);//4 days from now
+        console.log(data.list[33].dt_txt); //5 days from now
         //this is the 6th day in the future dont need this
-        console.log(data.list[6].dt_txt);
+        console.log(data.list[39].dt_txt);
+        console.log("***************************");
+        //this retrieves the noon time weather for 
+        //the next five days
+        for(let i = data.list.length -1-2; i >0; i-=8)
+        {
+            console.log(data.list[i].dt_txt);
+        }
 
     })
 }
